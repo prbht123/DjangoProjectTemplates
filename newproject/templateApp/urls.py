b163aa941fac555be0ef6bd0ef7402b0b40d647d
Piu De <piuwebkrone@gmail.com>
2022-12-09 10:10:08 +0530

    Implemented other templates

--- newproject/templateApp/urls.py
+++ newproject/templateApp/urls.py
@@ -3,5 +3,10 @@ from templateApp import views
 
 urlpatterns = [
 		path('service/', views.service),
+		path('e_brochure/', views.eBrochure),
+		path('career/', views.career),
+		path('error/', views.error),
+		path('default/', views.default),
+		path('', views.home),
 		
 ]
